56daa38099367575a273e61b1ced044c8a69552e
Nelson Idehara <idehara@yahoo.com>
2024-05-17 17:02:21 -0500

    R2
    Criação do TP2

---
+++ TP2/src/test/java/br/edu/infnet/spring_boot/TP2/Tp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.spring_boot.TP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
